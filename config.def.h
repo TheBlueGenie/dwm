069ae29dddbad8be9390b3ac82d2228c262faf2c
TheBlueGenie <thebluegenieuk@gmail.com>
2020-06-15 23:55:46 +0100

    Tweaks

--- config.def.h
+++ config.def.h
@@ -15,8 +15,8 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when the
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:size=10" };
-static const char dmenufont[]       = "Source Code Pro:size=10";
+static const char *fonts[]          = { "Source Code Pro:size=16" };
+static const char dmenufont[]       = "Source Code Pro:size=16";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -39,7 +39,7 @@ static const Rule rules[] = {
 	 */
 	/* class          instance    title           tags      mask  isfloating  isterminal  noswallow  monitor */
   { "Pavucontrol",    NULL,     NULL,           0,         1,        1,         1,        -1 },
-	{ "Gimp",           NULL,     NULL,           0,         1,        0,         0,        -1 },
+	{ "gimp",           NULL,     NULL,           8,         0,        0,        -1,        -1 },
 	{ "Firefox",        NULL,     NULL,           1 << 8,    0,        0,        -1,        -1 },
 	{ "st",             NULL,     NULL,           0,         0,        1,        -1,        -1 },
 	{ NULL,             NULL,     "Event Tester", 0,         1,        0,         1,        -1 }, /* xev */
